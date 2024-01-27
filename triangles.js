09af6f008949202556ce774a723daa943a4fc00c
chmtv <damik131@gmail.com>
2024-01-27 19:17:11 +0100

    final version test

--- triangles.js
+++ triangles.js
@@ -13,6 +13,9 @@ function CrossProductR3(vec1, vec2) {
     let z = vec1.x*vec2.y - vec1.y*vec2.x;
     return new PointR3(x,y,z);
 }
+function DotProductR3(vec1, vec2) {
+    return vec1.x*vec2.x + vec1.y*vec2.y + vec1.z*vec2.z;
+}
 
 function AreColinear(vec1,vec2) {
     let returnVal = true;
@@ -51,7 +54,7 @@ function triangleAreaFromPointsR3(p1,p2,p3) {
     
 }
 function angleBtwnVectorsR3(vec1, vec2) {
-    let dotProduct = vec1.x*vec2.x + vec1.y*vec2.y + vec1.z*vec2.z;
+    let dotProduct = DotProductR3(vec1,vec2);
     let magnitude1 = Math.sqrt(vec1.x*vec1.x + vec1.y*vec1.y + vec1.z*vec1.z);
     let magnitude2 = Math.sqrt(vec2.x*vec2.x + vec2.y*vec2.y + vec2.z*vec2.z);
 
@@ -100,31 +103,130 @@ function handleTriangleCoordsSubmit() {
         let AC = DistBtwnPointsR3(vec1, vec3);
         let BC = DistBtwnPointsR3(vec2, vec3);
         document.getElementById("triangleSideLengths").innerHTML = 
-        `Długości boków trójkąta: </br> |AB|=${AB} </br> |AC|=${AC} </br> |BC|=${BC}`;
+        `Długości boków trójkąta: </br> |AB|=${AB.toFixed(2)} </br> |AC|=${AC.toFixed(2)} </br> |BC|=${BC.toFixed(2)}`;
         // Triangle area
         let area = triangleAreaFromPointsR3(vec1,vec2,vec3);
-        document.getElementById("triangleArea").innerHTML = `Pole trójkąta: ${area}`
+        document.getElementById("triangleArea").innerHTML = `Pole trójkąta: ${area.toFixed(2)}`
         // Angles of the triangle
         let angleCAB = angleBtwnVectorsR3(vectorBtwnPointsR3(vec1,vec3), vectorBtwnPointsR3(vec1,vec2));
         let angleABC = angleBtwnVectorsR3(vectorBtwnPointsR3(vec3,vec2), vectorBtwnPointsR3(vec1,vec2));
         let angleBCA = angleBtwnVectorsR3(vectorBtwnPointsR3(vec1,vec3), vectorBtwnPointsR3(vec2,vec3));
-        document.getElementById("triangleAngles").innerHTML = `Kąt CAB: ${angleCAB} (${radToDeg(angleCAB)} stopni) </br> Kąt ABC: ${angleABC} (${radToDeg(angleABC)} stopni)</br> Kąt BCA: ${angleBCA} (${radToDeg(angleBCA)} stopni)`;
+        document.getElementById("triangleAngles").innerHTML = `Kąt CAB: ${angleCAB.toFixed(2)} (${radToDeg(angleCAB).toFixed(2)} stopni) </br> Kąt ABC: ${angleABC.toFixed(2)} (${radToDeg(angleABC).toFixed(2)} stopni)</br> Kąt BCA: ${angleBCA.toFixed(2)} (${radToDeg(angleBCA).toFixed(2)} stopni)`;
 
         if(radToDeg(angleABC) == 90) {
-            document.getElementById("triangleHeights").innerHTML = `Trójkąt jest prostokątny, co oznacza że ma tylko jedną wysokość (wychodzącą z AC): ${getTriangleHeight(area, AC)}`;
+            document.getElementById("triangleHeights").innerHTML = `Trójkąt jest prostokątny, co oznacza że ma tylko jedną wysokość (wychodzącą z AC): ${getTriangleHeight(area, AC).toFixed(2)}`;
         }
         else if(radToDeg(angleBCA) == 90) {
-            document.getElementById("triangleHeights").innerHTML = `Trójkąt jest prostokątny, co oznacza że ma tylko jedną wysokość (wychodzącą z AB): ${getTriangleHeight(area, AB)}`;
+            document.getElementById("triangleHeights").innerHTML = `Trójkąt jest prostokątny, co oznacza że ma tylko jedną wysokość (wychodzącą z AB): ${getTriangleHeight(area, AB).toFixed(2)}`;
         }
         else if(radToDeg(angleCAB) == 90) {
-            document.getElementById("triangleHeights").innerHTML = `Trójkąt jest prostokątny, co oznacza że ma tylko jedną wysokość (wychodzącą z BC): ${getTriangleHeight(area, BC)}`;
+            document.getElementById("triangleHeights").innerHTML = `Trójkąt jest prostokątny, co oznacza że ma tylko jedną wysokość (wychodzącą z BC): ${getTriangleHeight(area, BC).toFixed(2)}`;
         }
         else {
-            document.getElementById("triangleHeights").innerHTML = `Wysokości trójkąta: </br> z AC: ${getTriangleHeight(area, AC)} </br> z AB: ${getTriangleHeight(area, AC)} </br> z BC: ${getTriangleHeight(area, BC)}`;
+            document.getElementById("triangleHeights").innerHTML = `Wysokości trójkąta: </br> z AC: ${getTriangleHeight(area, AC).toFixed(2)} </br> z AB: ${getTriangleHeight(area, AC).toFixed(2)} </br> z BC: ${getTriangleHeight(area, BC).toFixed(2)}`;
         }
     }
 
-
-
     document.getElementById("trianglesMsg").innerHTML = resultHTML;
 }
+
+
+
+
+    // Tetrahedron logic
+    function getTetraCoordFromForm(coord) {
+        return Number(document.getElementsByName(`tetraCoord${coord}`)[0].value);
+    }
+    function isR3Zero(vec) {
+        return vec.x == 0 && vec.y == 0 && vec.z == 0;
+    }
+    function AreCoplanarThreePoints(vec1,vec2,vec3) {
+        let crossProduct = CrossProductR3(vec2,vec3);
+        let dotProduct = DotProductR3(vec1, crossProduct);
+        return dotProduct == 0;
+    }
+    function AreCoplanarFourPoints(vec1,vec2,vec3,vec4) {
+        let crossProduct = CrossProductR3(vectorBtwnPointsR3(vec1,vec2), vectorBtwnPointsR3(vec1,vec3));
+        let dotProduct = DotProductR3(vectorBtwnPointsR3(vec1,vec4), crossProduct);
+        return dotProduct == 0;
+    }
+    function handleTetraSubmit() {
+        let x1 = getTetraCoordFromForm("1X");
+        let y1 = getTetraCoordFromForm("1Y");
+        let z1 = getTetraCoordFromForm("1Z");
+        let vec1 = new PointR3(x1,y1,z1);
+
+        let x2 = getTetraCoordFromForm("2X");
+        let y2 = getTetraCoordFromForm("2Y");
+        let z2 = getTetraCoordFromForm("2Z");
+        let vec2 = new PointR3(x2,y2,z2);
+
+        let x3 = getTetraCoordFromForm("3X");
+        let y3 = getTetraCoordFromForm("3Y");
+        let z3 = getTetraCoordFromForm("3Z");
+        let vec3 = new PointR3(x3,y3,z3);
+
+        let x4 = getTetraCoordFromForm("4X");
+        let y4 = getTetraCoordFromForm("4Y");
+        let z4 = getTetraCoordFromForm("4Z");
+        let vec4 = new PointR3(x4,y4,z4);
+
+        let tetraVecs = [vec1,vec2,vec3,vec4];
+        let tetraBaseVecs = [];
+        let nonCoplanarVec;
+        let canMakeTetra = false;
+        for(let i = 0; i < tetraVecs.length; i++) {
+            vec = tetraVecs[i];
+            let vecsToCheck = [];
+            nonCoplanarVec = null;
+            for(let j = 0; j < tetraVecs.length; j++) {
+                
+                let _vec = tetraVecs[j];
+                // Add all the vectors that are not the currently checked one to an array to be checked later
+                if(i != j) {
+                    vecsToCheck.push(_vec);  
+                }
+                // Also set a variable of the other vector for later calculations
+                else {
+                    nonCoplanarVec = _vec;
+                }
+                // If there is a base and a non-coplanar vector set all the variables, in the other case say a message
+                if(vecsToCheck.length == 3 && nonCoplanarVec) {
+                    // Check if in the 4 vectors there are 3 that make a base (are coplanar) and if the other vector is not coplanar
+                    if(!AreCoplanarFourPoints(...vecsToCheck, nonCoplanarVec)) {
+                        tetraBaseVecs = vecsToCheck;
+                        canMakeTetra = true;
+                        break;
+                    }
+                }
+            }
+            if(canMakeTetra) break;
+        }
+        if(!canMakeTetra) {
+            document.getElementById("tetraMsg").innerHTML = "Wszystkie podane współrzędne są współpłaszczyznowe, nie można z nich utworzyć poprawnego czworościanu";
+            document.getElementById("tetraArea").innerHTML = "";
+            document.getElementById("tetraHeight").innerHTML = "";
+            document.getElementById("tetraVolume").innerHTML = "";
+            return;
+        }
+        // The tetrahedron parameters calculations
+        document.getElementById("tetraMsg").innerHTML = "Poniżej wypisane są wartości parametrów czworościanu utworzonego z podanych wierzchołków";
+        // Area
+        let sideArea1 = triangleAreaFromPointsR3(tetraBaseVecs[0],tetraBaseVecs[2],nonCoplanarVec);
+        let sideArea2 = triangleAreaFromPointsR3(tetraBaseVecs[0],tetraBaseVecs[1],nonCoplanarVec);
+        let sideArea3 = triangleAreaFromPointsR3(tetraBaseVecs[1],tetraBaseVecs[2],nonCoplanarVec);
+        let baseArea = triangleAreaFromPointsR3(...tetraBaseVecs);
+        let totalArea = sideArea1 + sideArea2 + sideArea3 + baseArea;
+        document.getElementById("tetraArea").innerHTML = `Pole powierzchni całkowitej: ${totalArea}`;
+        // Volume
+        let a = vectorBtwnPointsR3(tetraBaseVecs[0], tetraBaseVecs[1]);
+        let b = vectorBtwnPointsR3(tetraBaseVecs[0], tetraBaseVecs[2]);
+        let c = vectorBtwnPointsR3(tetraBaseVecs[0], nonCoplanarVec);
+        let volume = 1/6 * Math.abs(
+             DotProductR3(CrossProductR3(a,b), c)
+            );
+        document.getElementById("tetraVolume").innerHTML = `Objętość: ${volume}`;
+        // Height
+        let tetraHeight = volume / 1/3 * baseArea;
+        document.getElementById("tetraHeight").innerHTML = `Wysokość czworościanu: ${tetraHeight}`;
+        }
